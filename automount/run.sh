35960fd8d995a94a08da6eab63b7cd5147af4b3c
Dale Bewley <dbewley@redhat.com>
2025-02-26 15:32:23 -0800

    add force automount cleanup at start
    without this mounts will hang because they are still referencing a prior service IP

--- automount/run.sh
+++ automount/run.sh
@@ -3,13 +3,15 @@
 # Install automounter and nfs rpms
 dnf install --assumeyes \
   autofs \
-  nfs-utils \
+  nfs-utils
 #  openldap-clients # currently unused
 
 mkdir -p /mnt/automount
 
 # Start the autofs
 exec automount \
+  --force \
   --foreground \
+  --timeout 0 \
   --dont-check-daemon \
   --debug
