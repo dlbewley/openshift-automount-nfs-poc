8b18819f5dedcf18fcebd816d9131d7f9431c1e8
Dale Bewley <dbewley@redhat.com>
2025-02-26 07:14:37 -0800

    make automount less demanding

--- demo-script.sh
+++ demo-script.sh
@@ -14,8 +14,6 @@ DEMO_ROOT=$GIT_ROOT
 #unset zle_bracketed_paste
 clear
 
-p "# all the things"
-pei tree -L 3 $DEMO_ROOT
 pei "git remote -v"
 p
 
@@ -24,12 +22,11 @@ pei "oc apply -k $DEMO_ROOT/namespace"
 p "# 🔧 create nfs server"
 pei "oc apply -k $DEMO_ROOT/nfs-server"
 
-p "# ⌛ 🔍 wait for pod to start"
+p
+p "# ⌛ wait for pod to start"
 pei "oc wait pod -l app=nfs-server --for=condition=Ready=true"
 p "# 🔍 view pods"
 pei "oc get pods -o wide"
-
-#pei "oc apply -k $DEMO_ROOT/nfs-server"
 
 p "# 🔍 get service clusterIP"
 pei "oc get -n automount-nfs-poc svc -o wide"
@@ -37,13 +34,13 @@ pei "SERVICE_IP=$(oc get -n automount-nfs-poc svc/nfs-server --output jsonpath='
 pei "echo $SERVICE_IP"
 
 #pei "echo "* -rw ${SERVICE_IP}:/exports/&" > automount/extra.nfs"
-pei "echo '* -rw '${SERVICE_IP}':/exports/&' > automount/extra.nfs"
+pei "echo '* -rw,soft,intr '${SERVICE_IP}':/exports/&' > automount/extra.nfs"
 
 p
 p "# 🔧 create automount daemonset"
 pei "oc apply -k $DEMO_ROOT/automount"
 
-p "# ⌛ wait for pod to start"
+p "# ⌛ wait for pods to start"
 pei "oc wait pod -l app=automount --for=condition=Ready=true"
 p "# 🔍 view pods"
 pei "oc get pods -o wide"
@@ -56,10 +53,7 @@ pei "oc wait pod -l app=test-pod --for=condition=Ready=true"
 pei "POD=$(oc get pod -l app=test-pod -o name)"
 
 p "# 💻 view mounts in test pod"
-pei "oc rsh $POD ls /mnt/automount/dallas
-
-exit
-
-p "# 🎉 SUCCESS!"
+pei "oc rsh $POD findmnt"
+pei "oc rsh $POD ls /mnt/automount/dallas"
 
 exit
