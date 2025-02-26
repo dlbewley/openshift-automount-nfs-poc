13a7e9ea9402b19d9e8dc80d4262ed31e644eca4
Dale Bewley <dbewley@redhat.com>
2025-02-26 14:31:43 -0800

    use init as entry - do not attempt write

--- test-pod/init.sh
+++ test-pod/init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo Trying to write to data to an automounted volume
-echo $(date) Hello from $HOSTNAME >> /mnt/automount/dallas/$HOSTNAME
+#echo Trying to write to data to an automounted volume
+#echo $(date) Hello from $HOSTNAME >> /mnt/automount/dallas/$HOSTNAME
+
+ls /mnt/automount/dallas
+ls /mnt/automount/tucson
+ls /mnt/automount/sandiego
+df -h -t nfs
+sleep infinity
